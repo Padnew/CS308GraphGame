6ce7493819834e47fd174e0c47b14338d6369d84
Mitchell Connell <mitchell.connell@uni.strath.ac.uk>
2023-02-01 11:44:09 +0000

    feat: definition for edge

--- src/Edge.java
+++ src/Edge.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Edge {
+/**
+ * A definition of an edge, to be used within the adjacency list
+ * representing the graph
+ */
+public class Edge{
+    int destination;
+    int weight;
+
+    Edge(int destination, int weight){
+        this.destination = destination;
+        this.weight = weight;
+    }
 }
