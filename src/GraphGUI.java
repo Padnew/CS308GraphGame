53eabe73b6c7acfbbc4b2bf3d580bc5075e94ddd
Paddy <Paddygnewton@hotmail.co.uk>
2023-02-02 17:45:49 +0000

    Added GUI Boilerplate code

--- src/GraphGUI.java
+++ src/GraphGUI.java
@@ -1,5 +1,19 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GraphGUI {
+public class GraphGUI extends JFrame{
     private JPanel mainPanel;
+
+    public GraphGUI(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+    public static void main(String[] args) {
+        JFrame frame = new GraphGUI("Graph Game");
+        frame.setVisible(true);
+    }
 }
