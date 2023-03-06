0ead10c3a60310c3c43b3a52b88d1a9078cad0d7
Paddy <Paddygnewton@hotmail.co.uk>
2023-03-06 13:13:41 +0000

    Updated GUI Background

--- src/Game/GraphGUI.java
+++ src/Game/GraphGUI.java
@@ -5,15 +5,18 @@ import java.awt.*;
 public class GraphGUI extends JFrame{
     private JPanel mainFrame;
     private JTextField userInputTextField;
+    ImageIcon icon = new ImageIcon("data/Background.png");
     private JButton submitButton;
     private JLabel selectedNodeLabel;
     private JPanel graphPanel;
+    private JLabel backgroundLabel;
 
     public GraphGUI(){
         setTitle("AstroTraveller");
-
+        backgroundLabel.setIcon(new ImageIcon(new javax.swing.ImageIcon("data/Background.png").getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH)));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainFrame);
+        this.setLocationRelativeTo(null);
         this.setPreferredSize(new Dimension(800,600));
         this.pack();
     }
